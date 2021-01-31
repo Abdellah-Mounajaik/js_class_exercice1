40bc26ad7dffa0ae9906e8889c22f011f230d73e
Abdellah <mounajaikabdellah@gmail.com>
2021-01-31 01:50:41 +0100

    exo_fini

---
+++ public/js/main.js
@@ -0,0 +1,68 @@
+// exo1
+
+// class Personne {
+//     constructor(nom, prenom,age,taille) {
+//         this.nom = nom;
+//         this.prenom = prenom;
+//         this.age = age;
+//         this.taille = taille;
+//         this.phrase =() => {
+//             return `Bonjour, ${this.nom} ${this.prenom}, tu as ${this.age} et tu fais ${this.taille}`
+//         }
+//     }
+// }
+// let perso1 = new Personne ("mounaj", "abdel", 20, "1m78")
+// console.log(perso1);
+// let perso2 = new Personne ("dupont", "jean", 55, "1m65")
+// console.log(perso2);
+// let perso3 = new Personne ("dubois", "marc", 40, "1m85")
+// console.log(perso3);
+// console.log(perso1.phrase());
+// console.log(perso2.phrase());
+// console.log(perso3.phrase());
+
+// // exo2
+
+// class Personnage {
+//     constructor (age,nom,ville,) {
+//         this.age = age;
+//         this.nom = nom;
+//         this.ville = ville;
+//         this.sePresenter = () => {
+//             return `Bonjour, je m'appelle ${this.nom}`
+//         }
+//     }
+// }
+// let perso1 = new Personnage (20,"abdel","bruxelles")
+// console.log(perso1);
+// let perso2 = new Personnage (30,"jean","charleroi")
+// console.log(perso2);
+// console.log(perso1.sePresenter());
+
+// exo3
+
+class lieux {
+    constructor (nom,contenu) {
+        this.nom = nom;
+        this.contenu = [] ;
+    }
+}
+let maison = ("maison")
+console.log(maison);
+
+
+//  lieux
+// let maison = {
+//   nom: "maison",
+//   contenu: [],
+// };
+// let epicerie = {
+//   nom: "epicerie",
+//   contenu: [],
+//   ingredients: [],
+// };
+// let cuisine = {
+//   nom: "cuisine",
+//   contenu: [],
+//   ingredients: [],
+// };
